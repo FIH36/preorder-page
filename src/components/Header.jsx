0d40aa4f8b9fd5587b7211bc7832f4cab32628d9
DevSeulgi <DevSeulgi@gmail.com>
2025-03-21 15:57:38 +0900

    feat: product 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,34 @@ import styled from "@emotion/styled";
 import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  // 제품 데이터
+  const products = [
+    {
+      id: "하금테 스타일",
+      name: "하금테 스타일",
+      color: "Black",
+      price: 240000,
+      image: "/black_01.png", // 기존 이미지 경로 사용
+      quantity: 1,
+      type: "single",
+      subtitle: "세련된 감각을 담아 스마트함을 더한 하금테 프레임",
+    },
+    {
+      id: "스퀘어 스타일",
+      name: "스퀘어 스타일",
+      color: "Black",
+      price: 260000,
+      image: "/Black_Clear01.png", // 기존 이미지 경로 사용
+      quantity: 1,
+      type: "bundle",
+      subtitle: "클래식한 무드를 담아 신뢰감을 주는 블랙 프레임",
+    },
+  ];
+
   const navigate = useNavigate();
 
   const handleBuyNowClick = () => {
-    navigate("/purchase"); // '/purchase'는 PurchasePage 컴포넌트의 경로입니다
+    navigate("/purchase", { state: { product: products[1] } });
   };
 
   return (
