15f1c79a1600ad73625bde4f1f6c51f08988b8f7
DevSeulgi <DevSeulgi@gmail.com>
2025-03-27 01:34:02 +0900

    feat: toml 파일 추가 및 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import Purchase from "./pages/Purchase";
 import OrderComplete from "./pages/OrderComplete";
+import OrderResult from "./pages/order-result.jsx";
 import Home from "./pages/Home.jsx";
 import SEO from "./components/SEO.jsx";
 import "./styles/global.css";
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/purchase" element={<Purchase />} />
           <Route path="/order-complete" element={<OrderComplete />} />
+          <Route path="/order-result" element={<OrderResult />} />
         </Routes>
       </BrowserRouter>
     </HelmetProvider>
