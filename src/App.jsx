d89dd2f1b2c01f3e6bad650781652b90341c9609
DevSeulgi <DevSeulgi@gmail.com>
2025-04-22 09:16:43 +0900

    add: iframe page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 /** @jsxImportSource @emotion/react */
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {HelmetProvider} from "react-helmet-async";
-import Home from "./pages/Home.jsx";
+import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ChatUI from "./components/ChatUI.jsx";
 import SEO from "./components/SEO.jsx";
+import { I18nProvider } from "./contexts/I18nContext.jsx";
+import Home from "./pages/Home.jsx";
 import "./styles/global.css";
-import {I18nProvider} from "./contexts/I18nContext.jsx";
 
 function App() {
   return (
     <HelmetProvider>
       <I18nProvider>
-      <BrowserRouter>
-        <SEO />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <SEO />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* iframe용 페이지 */}
+            <Route path="/chat" element={<ChatUI />} />
+          </Routes>
+        </BrowserRouter>
       </I18nProvider>
     </HelmetProvider>
   );
